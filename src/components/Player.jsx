ebaeb920449c61f946b690e73023e69645744ae3
Jamprin12 <ortizmurciajeanpierre@gmail.com>
2020-06-26 09:19:49 -0500

    Copleted funcitions 	* Auto next song function 	* Resposive design improved 	* Styles completed for current app

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,21 +4,46 @@ import data from "../services/data.json";
 export default function Player() {
   // const [search, setSearch] = useState("lofi");
   const [songs, setSongs] = useState([]);
-  const [currentSong, setCurrentSong] = useState({});
+  const [currentSong, setCurrentSong] = useState(
+    data[Math.floor(Math.random() * data.length)]
+  );
   const [audio, setAudio] = useState(null);
   const [volume, setVolume] = useState(0);
 
   useEffect(() => {
+    setSongs(data);
     setAudio(document.querySelector("#song"));
     setVolume(document.querySelector("#volume").value / 100);
-    setSongs(data);
   }, []);
 
   const onLoading = () => {
     audio.volume = volume;
+    playSong();
   };
 
-  const nextSong = () => {
+  const prevSong = async () => {
+    if (!currentSong) return;
+
+    let song = songs.find((el) => el.id === currentSong.id - 1);
+
+    if (!song) return;
+
+    await setCurrentSong(song);
+    playSong();
+  };
+
+  const nextSong = async () => {
+    if (audio.currentTime === audio.duration) {
+      if (!currentSong) return;
+
+      let song = songs.find((el) => el.id === currentSong.id + 1);
+
+      if (!song) return;
+
+      await setCurrentSong(song);
+      playSong();
+    }
+
     if (!currentSong) return;
 
     let song = songs.find((el) => el.id === currentSong.id + 1);
@@ -28,31 +53,24 @@ export default function Player() {
     setCurrentSong(song);
     playSong();
   };
-  const prevSong = () => {
-    if (!currentSong) return;
 
-    let song = songs.find((el) => el.id === currentSong.id - 1);
-
-    if (!song) return;
-
-    setCurrentSong(song);
-    playSong();
-  };
-
-  const playSong = () => {
+  const playSong = async () => {
     if (!audio || !audio.src) return;
     document
       .querySelector(".button-toggle i")
       .classList.replace("fa-play", "fa-pause");
-    audio.play();
+
+    if (!audio.currentSrc) return;
+
+    await audio.play();
   };
 
-  const pauseSong = () => {
+  const pauseSong = async () => {
     if (!audio || !audio.src) return;
     document
       .querySelector(".button-toggle i")
       .classList.replace("fa-pause", "fa-play");
-    audio.pause();
+    await audio.pause();
   };
 
   const selectSong = (e) => {
@@ -86,7 +104,7 @@ export default function Player() {
           <button
             className="button-volume"
             onClick={() => {
-              document.querySelector("#volume").style.width = 10 + "vw";
+              document.querySelector("#volume").style.width = 20 + "vw";
             }}
             onDoubleClick={() => {
               document.querySelector("#volume").style.width = 0 + "vw";
@@ -111,6 +129,7 @@ export default function Player() {
           src={currentSong.url}
           id="song"
           onTimeUpdate={currentTime}
+          onEnded={nextSong}
         ></audio>
         <select className="select-song" onClick={selectSong}>
           {songs.map((song) => (
